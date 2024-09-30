0595a0fbf652ffd7cae312072414e2082c29f040
Mykel Angelo P. Nava <MykelAngelo.Nava@trajectorservices.com>
2024-09-30 20:02:12 +0800

    Final React

--- src/PokeReact.tsx
+++ src/PokeReact.tsx
@@ -41,7 +41,7 @@ const PokeReact = () => {
     <div className="container">
       <div className="pokemon-info">
         {pokemon1Data && (
-          <div>
+          <div className = "transparent-box">
             <p>Name: {pokemon1Data?.name}<br />
               Image: <img src={pokemon1Data?.sprites.front_default} className="image" /><br />
               Health: {pokemon1Data?.stats.find((s) => s.stat.name === "hp")?.base_stat}<br />
@@ -56,9 +56,9 @@ const PokeReact = () => {
       
       <div className="pokemon-info">
         {pokemon2Data && (
-          <div>
+          <div className="transparent-box">
             <p>Name: {pokemon2Data?.name}<br />
-              Image: <img src={pokemon2Data?.sprites.front_default} className="image" />
+              Image: <img src={pokemon2Data?.sprites.front_default} className="image" /><br />
               Health: {pokemon2Data?.stats.find((s) => s.stat.name === "hp")?.base_stat}<br />
               Damage: {pokemon1Data?.stats.find((s) => s.stat.name === "attack")?.base_stat}<br />
               Defense: {pokemon1Data?.stats.find((s) => s.stat.name === "defense")?.base_stat}<br />
@@ -70,9 +70,12 @@ const PokeReact = () => {
       <PinkButton buttonClick={fetchEnemyPokemon} label="Select" /><br/>
       <PinkButton buttonClick={battleresults} label={"Fight!"} />
       {hasFightResults && (
-        <div className="result">
+        <div className="transparent-box">
+          <p>
+          Pokemon Battle: <br/>
           {pokemon1Data?.name}'s HP after Damage: {pkmn1HP} <br/>
           {pokemon2Data?.name}'s HP after Damage: {pkmn2HP} <br/>
+          </p>
         </div>
       )}
     </div>
